a2bfd8c2c8d65debd92c92d6669ccc21b1430af0
nishimotoy <czy00347@nifty.com>
2026-09-04 17:09:13 +0900

    debug: AudioPlayerManager instance lifecycle

--- app/src/main/java/com/looplingua/app/player/audio/AudioPlayerManager.kt
+++ app/src/main/java/com/looplingua/app/player/audio/AudioPlayerManager.kt
@@ -1,5 +1,7 @@
 package com.looplingua.app.player.audio
 
+import android.util.Log
+
 object AudioPlayerManager {
 
     private val players =
@@ -7,13 +9,29 @@ object AudioPlayerManager {
 
     fun register(player: AudioPlayer) {
         players += player
+        Log.d(
+            "PLAYER_TRACE",
+            "AudioPlayerManager.register() " +
+                    "count=${players.size}"
+        )
     }
 
     fun unregister(player: AudioPlayer) {
         players -= player
+        Log.d(
+            "PLAYER_TRACE",
+            "AudioPlayerManager.unregister() " +
+                    "count=${players.size}"
+        )
     }
 
     fun stopAll() {
+        Log.d(
+            "PLAYER_TRACE",
+            "AudioPlayerManager.stopAll() " +
+                    "count=${players.size}"
+        )
+
         players.toList().forEach { player ->
             player.stop()
         }
